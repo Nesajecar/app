a464530806a049a35d8440064d893b49beefd70d
Nebojša Đekić <128518859+Nesajecar@users.noreply.github.com>
2025-07-06 20:29:03 +0200

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const PORT = process.env.PORT || 8080;
-
-var http = require('http');
-http.createServer(function(req,res){
-    res.writeHead(200,{'Content-Type':'text/html'});
-    res.write('i am MSEI')
-    res.end()
-
-}).listen(PORT,() => console.log(`Server aktivan... PORT ${PORT}`))
